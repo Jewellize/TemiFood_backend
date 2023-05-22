40c92f60b716f93a567f855d089077adf2b492a5
Jewellize <tatsuyakung.3@gmail.com>
2023-05-23 00:24:41 +0700

    undo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log(connection)
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/static/images')
+    cb(null, 'public/images')
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
