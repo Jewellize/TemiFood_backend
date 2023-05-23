85a51d8cb9790fa431fc896cb39c42f89032097c
Jewellize <tatsuyakung.3@gmail.com>
2023-05-23 15:33:09 +0700

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ console.log(connection)
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images')
+    cb(null, '/public/images')
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
